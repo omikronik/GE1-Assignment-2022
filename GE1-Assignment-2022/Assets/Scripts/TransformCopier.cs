890f2fa609274af39145dbc483c13612d46d59a9
Yasir Çeltik <55563251+omikronik@users.noreply.github.com>
2022-12-20 23:26:13 +0000

    fixed particle system tracking issue

--- GE1-Assignment-2022/Assets/Scripts/TransformCopier.cs
+++ GE1-Assignment-2022/Assets/Scripts/TransformCopier.cs
@@ -10,20 +10,19 @@ public class TransformCopier : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.SetParent(Target, false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.rotation = Target.rotation;
-        transform.LookAt(Target);
-
+        //transform. = Target.rotation;
+        transform.LookAt(Target, this.transform.parent.forward);
+        
         //transform.position = Target.position;
         //transform.localRotation = Quaternion.Euler(90f, 0f, 0f);
         //transform.localposition;
         //this.transform.position = target.transform.position;
-        //this.transform.position = this.transform.parent.position ;
+        this.transform.position = this.transform.parent.position ;
         //this.transform.rotation = this.transform.parent.rotation;
         //this.transform.eulerAngles= new Vector3(this.transform.parent.rotation.eulerAngles.x, this.transform.parent.eulerAngles.y + 90, this.transform.parent.eulerAngles.z);
         //this.transform.LookAt(this.transform.parent.position);
