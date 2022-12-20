797c20287a94f66a4156c505784ae3fcb6ec84f2
Yasir Çeltik <55563251+omikronik@users.noreply.github.com>
2022-12-20 23:26:32 +0000

    cleaning

--- GE1-Assignment-2022/Assets/Scripts/TransformCopier.cs
+++ GE1-Assignment-2022/Assets/Scripts/TransformCopier.cs
@@ -15,18 +15,7 @@ public class TransformCopier : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform. = Target.rotation;
         transform.LookAt(Target, this.transform.parent.forward);
-        
-        //transform.position = Target.position;
-        //transform.localRotation = Quaternion.Euler(90f, 0f, 0f);
-        //transform.localposition;
-        //this.transform.position = target.transform.position;
         this.transform.position = this.transform.parent.position ;
-        //this.transform.rotation = this.transform.parent.rotation;
-        //this.transform.eulerAngles= new Vector3(this.transform.parent.rotation.eulerAngles.x, this.transform.parent.eulerAngles.y + 90, this.transform.parent.eulerAngles.z);
-        //this.transform.LookAt(this.transform.parent.position);
-        //this.transform.eulerAngles = new Vector3(target.transform.eulerAngles.x, target.transform.eulerAngles.x, target.transform.eulerAngles.z);
-        //this.transform.eulerAngles = new Vector3(target.transform.eulerAngles.x, target.transform.eulerAngles.x, target.transform.eulerAngles.z);
     }
 }
